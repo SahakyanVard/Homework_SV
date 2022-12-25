2e7a372f2a8029ae4d00fbf7ebc7f2006535746b
SahakyanVard <sahakyan2023@gmail.com>
2022-12-25 19:33:32 +0400

    ADD NEW FILES

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+        // homework
+
+        // 1 SECTION
+
+        int c, a, b;
+        a = 1;
+        b = 5;
+        c = a * a + b * b;
+        System.out.println("c = " + c);
+
+        // 2 SECTION
+
+        a = 2;
+        b = 4;
+        c = 6;
+        int s = (a + b) / 2 * c;
+        System.out.println("table surface s = " + s);
+
+        // 3 SECTION
+
+        int h = 579;
+        System.out.println(h / 100);
+        System.out.println((h % 100 - h % 10) / 10);
+        System.out.println(h % 10);
+    }
+}
