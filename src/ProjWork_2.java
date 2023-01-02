7543da570a1b265bb3ad40d28f7f17559216aeb2
SahakyanVard <sahakyan2023@gmail.com>
2023-01-03 00:02:14 +0400

    UPDATE src FILE

--- src/ProjWork_2.java
+++ src/ProjWork_2.java
@@ -1,15 +1,18 @@
 public class ProjWork_2 {
     public static void main(String[] args) {
 
-        // First issue (==) /integer number/
-//        int x, y;
-//        x = 25;
-//        y = 5;
-//        System.out.println(x / y == 5);
-//        System.out.println(x / y != 5);
-//
-//        // Second issue   arithmetic average /integer number/
-//
+         // First issue (==) /integer number/
+        int x, y, z;
+        x = 888;
+        y = -8;
+        z = x / y;
+        System.out.println(x == z * y);
+        x = -8;
+        y = 888;
+        System.out.println(x == z * y);
+
+        // Second issue   arithmetic average /integer number/
+
 //        int a, b, c, d, e, f;
 //        a = 7;
 //        b = 1;
@@ -31,39 +34,39 @@ public class ProjWork_2 {
         // SECOND VERSION
 
         // First issue (==) /integer number/
-        byte x;
-        long y;
-
-        x = 20;
-        y = -1920000000000L;
-        System.out.println(x / y == 0);
-        System.out.println(x / y != 0);
-
-        // Second issue   arithmetic average /integer number/
-
-        byte a;
-        int b;
-        short c;
-        long d;
-        int e;
-        long f;
-        a = 50;
-        b = -78;
-        c = -100;
-        double m1 = (double) (a + b + c) / 3;
-        System.out.println(m1);
-        d = -789789L;
-        e = 555;
-        f = 579579L;
-
-        double m2 = (double) (d + e + f) / 3;
-        System.out.println(m2);
-        long sum = (long) (m1 + m2);
-        System.out.println(sum);
-
-        // Third issue /double in decimal/
-        double d1 = 88.777;
-        System.out.println((float)(d1-(int)d1));
+//        byte x;
+//        long y;
+//
+//        x = 20;
+//        y = -1920000000000L;
+//        System.out.println(x / y == 0);
+//        System.out.println(x / y != 0);
+//
+//        // Second issue   arithmetic average /integer number/
+//
+//        byte a;
+//        int b;
+//        short c;
+//        long d;
+//        int e;
+//        long f;
+//        a = 50;
+//        b = -78;
+//        c = -100;
+//        double m1 = (double) (a + b + c) / 3;
+//        System.out.println(m1);
+//        d = -789789L;
+//        e = 555;
+//        f = 579579L;
+//
+//        double m2 = (double) (d + e + f) / 3;
+//        System.out.println(m2);
+//        long sum = (long) (m1 + m2);
+//        System.out.println(sum);
+//
+//        // Third issue /double in decimal/
+//        double d1 = 88.777;
+//        System.out.println((float)(d1-(int)d1));
 
 
 
