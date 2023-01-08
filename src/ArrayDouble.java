0b74b6d16bf4a39484ff76e607366cdce3c4a147
SahakyanVard <sahakyan2023@gmail.com>
2023-01-08 22:57:20 +0400

    UPDATE JAVA FAIL

--- src/ArrayDouble.java
+++ src/ArrayDouble.java
@@ -2,18 +2,13 @@ import java.util.Arrays;
 public class ArrayDouble {
     public static void main(String[] args) {
         // Array of non-negative numbers.
-        double[] firstArray = {33.3, 55.5, -77.7, -88.8, -99.9};
+        double[] firstArray = {-33.3, 55.5, -77.7, -88.8, -99.9};
         System.out.println(Arrays.toString(firstArray));
         double[] nonNegativeNumbers = new double[5];
-        System.out.print((firstArray[0] >= 0 ? firstArray[0] : nonNegativeNumbers[0]) + " ");
-        System.out.print((firstArray[1] >= 0 ? firstArray[1] : nonNegativeNumbers[1]) + " ");
-        System.out.print((firstArray[2] >= 0 ? firstArray[2] : nonNegativeNumbers[2]) + " ");
-        System.out.print((firstArray[3] >= 0 ? firstArray[3] : nonNegativeNumbers[3]) + " ");
-        System.out.print((firstArray[4] >= 0 ? firstArray[4] : nonNegativeNumbers[4]) + " ");
-
-        nonNegativeNumbers[0] = firstArray[0];
-        nonNegativeNumbers[1] = firstArray[1];
-        System.out.println("\n");
-        System.out.println(Arrays.toString(nonNegativeNumbers));
-}
+        System.out.print("{" + (firstArray[0] >= 0 ? nonNegativeNumbers[0] = firstArray[0] : nonNegativeNumbers[0]) + " ");
+        System.out.print((firstArray[1] >= 0 ? nonNegativeNumbers[1] = firstArray[1] : nonNegativeNumbers[1]) + " ");
+        System.out.print((firstArray[2] >= 0 ? nonNegativeNumbers[2] = firstArray[2] : nonNegativeNumbers[2]) + " ");
+        System.out.print((firstArray[3] >= 0 ? nonNegativeNumbers[3] = firstArray[3] : nonNegativeNumbers[3]) + " ");
+        System.out.print((firstArray[4] >= 0 ? nonNegativeNumbers[4] = firstArray[4] : nonNegativeNumbers[4]) + "}");
+    }
 }
