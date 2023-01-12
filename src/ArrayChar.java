12999dfb3d20797fbebb70be2c54a67651901af8
SahakyanVard <sahakyan2023@gmail.com>
2023-01-13 00:21:56 +0400

    UPDATE JAVA FILE

--- src/ArrayChar.java
+++ src/ArrayChar.java
@@ -3,11 +3,11 @@ public class ArrayChar {
         char[] charArray = {'E', 'N', 'D', 'I', 'N', 'G'};
         // search for and find input 'A'
         int index = 0;
-        while (index < charArray.length - 1) {
+        while (index < charArray.length) {
             if (charArray[index] == 'A') {
                 break;
             } else index++;
         }
-        System.out.println(charArray[index] == 'A' ? "'A' symbol index is " + index : "'A' symbol is not find.");
+        System.out.println(index == charArray.length && charArray[index-1] != 'A' ? "'A' symbol is not find." : "'A' symbol index is " + index);
     }
 }
