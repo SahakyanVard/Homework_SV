7d772de66e2e4f5eb6f31a1ddbfe8ef58bb6bf89
SahakyanVard <sahakyan2023@gmail.com>
2023-01-20 12:48:50 +0400

    UPDATE JAVA FAILE

--- src/PrimeNumberArray.java
+++ src/PrimeNumberArray.java
@@ -1,9 +1,9 @@
 public class PrimeNumberArray {
     public static void main(String[] args) {
-        int[] positive = {45, 2, 1, 88, 5, 7, 44};
-
+        int[] positive = {44, 55, 66, 88, 99,100};
+        int number = 1;
         for (int n1 = 0; n1 < positive.length; n1++) {
-            int number = 0;
+            number = 0;
             for (int n2 = 2; n2 <= positive[n1] / 2; n2++) {
                 if (positive[n1] % n2 == 0) {
                     number = 1;
@@ -13,5 +13,6 @@ public class PrimeNumberArray {
             System.out.print(number == 0 ? positive[n1] + " " : "");
         }
         System.out.println();
+        System.out.print(number == 1 ? "The prime number is not find." : "");
     }
 }
