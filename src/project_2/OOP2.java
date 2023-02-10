a9eec96e8467d0abd3a9082cbf616aa909b6d185
SahakyanVard <sahakyan2023@gmail.com>
2023-02-10 17:40:36 +0400

    ADD NEW FILE

--- src/project_2/OOP2.java
+++ src/project_2/OOP2.java
@@ -1,9 +1,16 @@
 package project_2;
-
+import java.util.Scanner;
 public class OOP2 {
     public static void main(String[] args) {
+        int matrix[][]={{9,9,9},{1,1,1},{2,2}, {100,200,300,400,500}};
+        matrixArrayMethod1(matrix);
+    }
 
-
-
+    public static void matrixArrayMethod1(int matrix[][]) {
+        for (int i = 0; i < matrix.length ; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j]+" ");
+            }
+        }
     }
 }
