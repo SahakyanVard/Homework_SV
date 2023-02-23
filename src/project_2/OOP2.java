607a2ca46e1599f32fc5bc0c9d2cd2b467b2597e
SahakyanVard <sahakyan2023@gmail.com>
2023-02-23 21:52:55 +0400

    UPDATE FILE3

--- src/project_2/OOP2.java
+++ src/project_2/OOP2.java
@@ -1,14 +1,20 @@
 package project_2;
 public class OOP2 {
     public static void main(String[] args) {
-        int matrix[][] = {{9},
-                          {1, 1},
-                          {2, 2, 2}};
+        int matrix[][] = {{0,1},
+                          {2, 3},
+                          {4, 5, 6,7},
+                          {8,9}};
+
         matrixArrayMethod1(matrix);
    }
     public static void matrixArrayMethod1(int matrix[][]) {
         int element = 0;
-        int length=matrix[2].length+matrix[0].length+matrix[1].length;
+        int length=0;
+        for (int i = 0; i < matrix.length ; i++) {
+            length+=matrix[i].length;
+        }
+
         int[] array = new int[length];
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
