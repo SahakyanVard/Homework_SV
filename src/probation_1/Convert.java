2db0784db1cd903041175faf955b486a61f1fe5b
SahakyanVard <sahakyan2023@gmail.com>
2023-04-20 12:16:59 +0400

    ADD NEW FILE

---
+++ src/probation_1/Convert.java
@@ -0,0 +1,10 @@
+package probation_1;
+
+public class Convert {
+    public static void main(String[] args) {
+        // CONVERT STRING TO FLOAT.
+        String s = "77.8";
+        float f = Float.parseFloat("77.8");
+        System.out.println(f);
+    }
+}
